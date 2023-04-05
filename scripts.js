0dda11573c297768f804b3b231039d992ab45924
Luis C <sir.kant@gmail.com>
2023-04-05 21:20:19 +0200

    Generated JavaScript code

---
+++ scripts.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html> 
+<html lang="en"> 
+  <head> 
+    <meta charset="UTF-8"> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"> 
+    <title>Website Title</title> 
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script> 
+  </head> 
+  <body> 
+    <nav class="navbar navbar-expand-sm navbar-light bg-light"> 
+      <a href="#" class="navbar-brand">Website Logo</a> 
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation"> 
+        <span class="navbar-toggler-icon"></span> 
+      </button> 
+      <div class="collapse navbar-collapse" id="navbarNav"> 
+        <ul class="navbar-nav"> 
+          <li class="nav-item"><a href="#" class="nav-link">Home</a></li> 
+          <li class="nav-item"><a href="#" class="nav-link">About</a></li> 
+          <li class="nav-item"><a href="#" class="nav-link">Contact</a></li> 
+        </ul> 
+      </div> 
+    </nav> 
+
+    <div> 
+      <!-- Page content goes here --> 
+    </div> 
+  </body> 
+</html>
