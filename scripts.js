f0414ab4d0609fb982a0ff6ee732c96def303d9f
Luis Caceres <sir.kant@gmail.com>
2023-04-07 21:22:23 +0200

    Generated JavaScript code

--- scripts.js
+++ scripts.js
@@ -1,46 +1,25 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Example Website</title>
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1WmJW/JmZQ5stwEULTy" crossorigin="anonymous">
-    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha384-7iHm6aGFYSO03h2ZrFJHX07cXdX5LgIPiJlxegdGKj3NsSWGiHoW8lxxxGDjGe" crossorigin="anonymous"></script>
-    
+    <title>My Website</title>
+    <meta charset="utf-8">
+    <!-- Latest compiled and minified CSS for Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap-theme.min.css">
 </head>
+    
 <body>
     
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-md navbar-light bg-light">
-        <a class="navbar-brand" href="#">Example Website</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle Navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarContent">
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">About Us</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Contact Us</a>
-                </li>
-            </ul>
-        </div>
+    <!-- Responsive Navbar -->  
+    <nav class="navbar navbar-expand-md bg-dark navbar-dark">
+       Bootstrap v4.6.0
     </nav>
- 
-    <!-- Content -->
-    <div class="container-fluid mt-3">
-        <p>Your content here...</p>
-        <p>Here we can have whatever content that is appropriate for our website.</p>
-    </div>
     
-    <!-- Scripts -->
-    <script>
-        // Your scripts here
-    </script>
-</body>
+    <!-- JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js"></script>
+    
+    </body>
 </html>
