b4c3f3e9b8406f43095a7a10f601d295e4819ba9
Luis Caceres <sir.kant@gmail.com>
2023-04-07 13:42:44 +0200

    Generated JavaScript code

--- scripts.js
+++ scripts.js
@@ -1,1 +1,35 @@
-console.log('Hello, World!');
+<html>
+	<head>
+   		<title>My Website</title>
+   		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+   		<link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet">
+	</head>
+	<body>
+   		<nav class="navbar navbar-default">
+      		<div class="container">
+         		<div class="navbar-header">
+            		<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+               			<span class="sr-only">Toggle navigation</span>
+               			<span class="icon-bar"></span>
+               			<span class="icon-bar"></span>
+               			<span class="icon-bar"></span>
+            		</button>
+         		</div>
+         		<div id="navbar" class="collapse navbar-collapse">
+            		<ul class="nav navbar-nav">
+               			<li class="active"><a href="index.html">Home</a></li>
+               			<li><a href="about.html">About</a></li>
+               			<li><a href="contact.html">Contact</a></li>
+            		</ul>
+         		</div><!--/.nav-collapse -->
+      		</div>
+   		</nav>
+   
+   		<div class="container">
+	
+		</div><!-- /.container -->
+   
+   		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+   		<script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	</body>
+</html>
