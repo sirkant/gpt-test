5192f61745d941ab37559aaae65ab48160bf0df3
Luis Caceres <sir.kant@gmail.com>
2023-04-07 20:59:53 +0200

    Generated JavaScript code

--- scripts.js
+++ scripts.js
@@ -1,48 +1,40 @@
 <!DOCTYPE html>
-<html>
-  <head>
-	<title>My Website</title>
-	<meta charset="utf-8">
-	<!-- Include the latest version of Bootstrap -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-	<style>
-	
-	</style>
-	<script>
-	// Here you can add any JavaScript which controls the navigation menu
-	</script>
-  </head>
-  <body> 
-	<!-- Responsive Navbar -->
-	<nav class="navbar navbar-default navbar-fixed-top">
-	  <div class="container-fluid">
-		<div class="navbar-header">
-		  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		  </button>
-		  <a class="navbar-brand" href="#">My Website</a>
-		</div>
-		<div class="collapse navbar-collapse" id="myNavbar">
-		  <ul class="nav navbar-nav">
-			<li><a href="#">Home</a></li>
-			<li><a href="#">About Us</a></li>
-			<li><a href="#">Contact Us</a></li>
-		  </ul>
-		  <form class="navbar-form navbar-right">
-		    <div class="form-group">
-			  <input type="text" class="form-control" placeholder="Search...">
-			</div>
-		    <button type="submit" class="btn btn-default">Submit</button>
-		  </form>
-		</div>
-	  </div>	
-	</nav>
-	
-	<!-- Your content here -->
-	
-  </body>
+<html> 
+<head>
+    <meta charset="utf-8">
+    <title>My website</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <script> 
+    $(document).ready(function() {
+        $('.nav-link').click(function() {
+            $('.active').removeClass('active');
+            $(this).addClass('active');
+        });
+    }); 
+</script>
+</head>
+<body>
+    <nav class="navbar navbar-default navbar-expand-lg navbar-light bg-light">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="#">My Website </a>
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span> 
+            </button>
+        </div>
+        <div class="collapse navbar-collapse" id="myNavbar">
+            <ul class=" nav navbar-nav">
+                <li><a class="nav-link active" href="#Home">Home</a></li>
+                <li><a class="nav-link" href="#About">About us</a></li>
+                <li><a class="nav-link" href="#Contact">Contact us</a></li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container">
+        <!-- content goes here -->
+    </div>
+</body>
 </html>
