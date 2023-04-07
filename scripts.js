17248be8cae57b7124d020c26f2cf3c6a843c39c
Luis Caceres <sir.kant@gmail.com>
2023-04-07 21:16:54 +0200

    Generated JavaScript code

--- scripts.js
+++ scripts.js
@@ -1,40 +1,46 @@
 <!DOCTYPE html>
-<html> 
+<html lang="en">
 <head>
-    <meta charset="utf-8">
-    <title>My website</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-    <script> 
-    $(document).ready(function() {
-        $('.nav-link').click(function() {
-            $('.active').removeClass('active');
-            $(this).addClass('active');
-        });
-    }); 
-</script>
+    <meta charset="UTF-8">
+    <title>Example Website</title>
+    
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1WmJW/JmZQ5stwEULTy" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.6.0/js/bootstrap.min.js" integrity="sha384-7iHm6aGFYSO03h2ZrFJHX07cXdX5LgIPiJlxegdGKj3NsSWGiHoW8lxxxGDjGe" crossorigin="anonymous"></script>
+    
 </head>
 <body>
-    <nav class="navbar navbar-default navbar-expand-lg navbar-light bg-light">
-        <div class="navbar-header">
-            <a class="navbar-brand" href="#">My Website </a>
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span> 
-            </button>
-        </div>
-        <div class="collapse navbar-collapse" id="myNavbar">
-            <ul class=" nav navbar-nav">
-                <li><a class="nav-link active" href="#Home">Home</a></li>
-                <li><a class="nav-link" href="#About">About us</a></li>
-                <li><a class="nav-link" href="#Contact">Contact us</a></li>
+    
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-md navbar-light bg-light">
+        <a class="navbar-brand" href="#">Example Website</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent" aria-controls="navbarContent" aria-expanded="false" aria-label="Toggle Navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarContent">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">About Us</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Contact Us</a>
+                </li>
             </ul>
         </div>
     </nav>
-    <div class="container">
-        <!-- content goes here -->
+ 
+    <!-- Content -->
+    <div class="container-fluid mt-3">
+        <p>Your content here...</p>
+        <p>Here we can have whatever content that is appropriate for our website.</p>
     </div>
+    
+    <!-- Scripts -->
+    <script>
+        // Your scripts here
+    </script>
 </body>
 </html>
